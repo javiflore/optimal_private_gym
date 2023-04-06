4b9b77e9c6af7107de118f9b8d903f42e33f0a07
Francisco Javier Florencio Airado <fjflorenc@gmail.com>
2023-04-06 11:02:58 +0200

    pagina inicio

--- src/app/app-routing.module.ts
+++ src/app/app-routing.module.ts
@@ -1,7 +1,15 @@
 import { NgModule } from '@angular/core';
 import { RouterModule, Routes } from '@angular/router';
+import { EquipoComponent } from './components/equipo/equipo.component';
+import { InicioComponent } from './components/inicio/inicio.component';
+import { TarifasComponent } from './components/tarifas/tarifas.component';
 
-const routes: Routes = [];
+const routes: Routes = [
+  { path: 'inicio', component: InicioComponent },
+  { path: 'equipo', component: EquipoComponent },
+  { path: 'tarifas', component: TarifasComponent },
+  { path: '**', redirectTo: '/inicio', pathMatch: 'full' }
+];
 
 @NgModule({
   imports: [RouterModule.forRoot(routes)],
