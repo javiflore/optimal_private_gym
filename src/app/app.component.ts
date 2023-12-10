1a9f13935f4b1550964a8dd83bad9b990a61d47c
Francisco Javier Florencio Airado <fjflorenc@gmail.com>
2023-12-10 14:02:38 +0100

    ADD - card cookies

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -7,4 +7,19 @@ import { Component } from '@angular/core';
 })
 export class AppComponent {
   title = 'optimal_private_gym';
+  cardCookie: HTMLElement | null | undefined;
+
+  constructor(){}
+
+  ngOnInit() {
+    this.cardCookie = document.getElementById('cardCookie');
+  }
+
+  cerrarCookies() {
+    if (this.cardCookie) {
+      this.cardCookie.style.display = 'none';
+    } else {
+      console.error('El elemento cardCookie no se ha encontrado');
+    }
+  }
 }
